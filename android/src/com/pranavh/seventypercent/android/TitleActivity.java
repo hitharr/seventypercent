8c9de9075bab88a1cb60a4523a98afe4d5c7b209
hitharr <hitharr@users.noreply.github.com>
2015-05-26 12:06:59 -0500

    Update TitleActivity.java
    comments

--- android/src/com/pranavh/seventypercent/android/TitleActivity.java
+++ android/src/com/pranavh/seventypercent/android/TitleActivity.java
@@ -1,5 +1,13 @@
+/**
+ * The TitleActivity Class creates the title screen of the app
+ * @author Ashley Handoko, Pranav Harathi, Brandon Perry
+ * Period: 3
+ * Date: 5-26-15
+ */
+
 package com.pranavh.seventypercent.android;
 
+//import statements
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -15,6 +23,7 @@ public class TitleActivity extends Activity {
 
     private static final String TAG = "statusMessages";
 
+    /*Adds the font typeface*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,69 +35,79 @@ public class TitleActivity extends Activity {
         Log.i(TAG, "onCreate");
     }
 
+   /* Overrides and call the onStart superclass*/
     @Override
     protected void onStart() {
         super.onStart();
         Log.i(TAG, "onStart");
     }
 
-
+    /*Overrides and calls the onResume superclass*/
     @Override
     protected void onResume() {
         super.onResume();
         Log.i(TAG, "onResume");
     }
 
-
+  /*  Overrides and call the onPause superclass*/
     @Override
     protected void onPause() {
         super.onPause();
         Log.i(TAG, "onPause");
     }
 
-
+    /*   Overrides and call the onStop superclass*/
     @Override
     protected void onStop() {
         super.onStop();
         Log.i(TAG, "onStop");
     }
 
-
+    /*   Overrides and call the onRestart superclass*/
     @Override
     protected void onRestart() {
         super.onRestart();
         Log.i(TAG, "onRestart");
     }
 
-
+     /*   Overrides and call the onDestroy superclass*/
     @Override
     protected void onDestroy() {
         super.onDestroy();
         Log.i(TAG, "onDestroy");
     }
 
-
+     /*   Overrides and call the onSaveInstanceState superclass
+     *    @param outState bundle to call super
+     */
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
         Log.i(TAG, "onSaveInstanceState");
     }
 
-
+     /*   Overrides and call the onRestoreInstanceState superclass
+     *    @param savedInstanceState Bundle to call super class  
+     */
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
         Log.i(TAG, "onRestoreInstanceState");
     }
 
-
+    /*Override onCreateOptionsMenu class, inflates the menu and adds items to the action bar if it is present.
+    * @param menu Meny to inflate
+    */
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        
         getMenuInflater().inflate(R.menu.menu_title, menu);
         return true;
     }
 
+    /*Override onOptionsItemSelected class, handles action bar clicks
+    * @param item MenuItem to retrieve id
+    */
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
@@ -104,6 +123,10 @@ public class TitleActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
+
+    /*Starts the activity
+    * @param view View to be used
+    */
     public void startGame(View view) {
         Intent intent = new Intent(this, AndroidLauncher.class);
         startActivity(intent);
